5b5b508feaf3c8398eb922b429ac7aca9f73ed10
John Fowler <johnfowler@johns-mbp.myfiosgateway.com>
2020-11-07 12:35:05 -0500

    updated to use different urls

--- app.js
+++ app.js
@@ -33,23 +33,24 @@ let response = '';
 
 const puppeteer = require('puppeteer');  
 
-(async () => {                              
-	const browser = await puppeteer.launch();   
-	const page = await browser.newPage();  
-	const url = 'https://twitter.com/kurteichenwald/status/1304228683067121666';
-	const imgPath = url.split('/')[3] + '-' + url.split('/')[5]         
-	await page.goto(url); 
-
-	await page.waitForSelector('article.css-1dbjc4n.r-18u37iz.r-1ny4l3l');    
-	const element = await page.$('article.css-1dbjc4n.r-18u37iz.r-1ny4l3l');  
-	await element.screenshot({path: 'public/screenshots/' + imgPath + '.png'});
-	response = 'screenshots/' + imgPath + '.png';
-	await browser.close();                         
-})();
-
 app.get('/url', (req, res, next) => {
+	(async () => {                              
+		const browser = await puppeteer.launch();   
+		const page = await browser.newPage();  
+		const url = req._parsedUrl.query.split('=')[1];
+		const imgPath = url.split('/')[3] + '-' + url.split('/')[5]         
+		await page.goto(url); 
+
+		await page.waitForSelector('article.css-1dbjc4n.r-18u37iz.r-1ny4l3l');    
+		const element = await page.$('article.css-1dbjc4n.r-18u37iz.r-1ny4l3l');  
+		await element.screenshot({path: 'public/screenshots/' + imgPath + '.png'});
+		response = 'screenshots/' + imgPath + '.png';
+		await browser.close();                         
+	})();
 	res.send(`<img src="${response}">`)
 });
+
+
 
 
 
