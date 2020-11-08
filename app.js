514a4758b8d3b40a9eeb1d46006ee4cc334be5ff
John Fowler <johnfowler@Johns-MacBook-Pro.local>
2020-11-08 13:21:30 -0500

    updated get and post routes

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const puppeteer = require('puppeteer')
 
 const app = express()
 const port = 3000
@@ -8,57 +9,66 @@ app.listen(port, () => {
 })
 
 app.use(express.static('public'))
-let response = '';
 
-// const https = require('https');
-// const nasaURL = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY';
+app.post('/', (req, res) => {
+	
+	req.on('data', function(chunk) {
+			const formdata = chunk.toString()
+			
+			const url = decodeURIComponent(formdata.split('=')[1])
 
-// https.get(nasaURL, (resp) => {
-// 	let data = '';
+			let form = ''
+			let result = ''  
 
-// 	// A chunk of data has been recieved.
-// 	resp.on('data', (chunk) => {
-// 		data += chunk;
-// 	});
+			async function screenshot() {  
+				const browser = await puppeteer.launch()   
+				const page = await browser.newPage()  
+				const imgPath = url.split('/')[3] + '-' + url.split('/')[5]         
+				await page.goto(url) 
+				
+				await page.waitForSelector('article.css-1dbjc4n.r-18u37iz.r-1ny4l3l') 
 
-// 	// The whole response has been received. Print out the result.
-// 	resp.on('end', () => {
-// 		response = JSON.parse(data).explanation;
-// 		console.log(JSON.parse(data).explanation);
-// 	});
+				let div_selector_to_remove = ".css-1dbjc4n.r-aqfbo4.r-14lw9ot.r-my5ep6.r-rull8r.r-qklmqi.r-gtdqiz.r-ipm5af.r-1g40b8q";
+				await page.evaluate((sel) => {
+				    var elements = document.querySelectorAll(sel);
+				    for(var i=0; i< elements.length; i++){
+				        elements[i].parentNode.removeChild(elements[i]);
+				    }
+				}, div_selector_to_remove)
 
-// }).on("error", (err) => {
-// 	console.log("Error: " + err.message);
-// });
+				let div_selector_to_remove1 = ".css-1dbjc4n.r-aqfbo4.r-1p0dtai.r-1d2f490.r-12vffkv.r-1xcajam.r-zchlnj";
+				await page.evaluate((sel) => {
+				    var elements = document.querySelectorAll(sel);
+				    for(var i=0; i< elements.length; i++){
+				        elements[i].parentNode.removeChild(elements[i]);
+				    }
+				}, div_selector_to_remove1)
 
-const puppeteer = require('puppeteer');  
+				const element = await page.$('article.css-1dbjc4n.r-18u37iz.r-1ny4l3l')  
+				await element.screenshot({path: 'public/screenshots/' + imgPath + '.png'})
+				result = 'screenshots/' + imgPath + '.png'
+				await browser.close()  
 
-app.get('/url', (req, res, next) => {
-	(async () => {                              
-		const browser = await puppeteer.launch();   
-		const page = await browser.newPage();  
-		const url = req._parsedUrl.query.split('=')[1];
-		const imgPath = url.split('/')[3] + '-' + url.split('/')[5]         
-		await page.goto(url); 
+				form = `<!DOCTYPE html>
+					<html lang="en">
+						<head>
+							<meta charset="UTF-8"> 
+							<title>TweetShot</title>
+						</head>
+						<body>
+							<form name="tweetshot" action="/" method="post">
+								<input type="text" name="twurl" value="${url}">
+								
+								<input type="submit" value="Submit">
+							</form>
+							<img src="${result}">
+						</body>
+					</html>`   
 
-		await page.waitForSelector('article.css-1dbjc4n.r-18u37iz.r-1ny4l3l');    
-		const element = await page.$('article.css-1dbjc4n.r-18u37iz.r-1ny4l3l');  
-		await element.screenshot({path: 'public/screenshots/' + imgPath + '.png'});
-		response = 'screenshots/' + imgPath + '.png';
-		await browser.close();                         
-	})();
-	res.send(`<img src="${response}">`)
+				res.setHeader('Content-Type', 'text/html')
+				res.writeHead(200)
+				res.end(form)                 
+			};
+			screenshot()
+		})
 });
-
-
-
-
-
-
-// app.get('/', (req, res) => {
-// 	res.send('Hello World!')
-// })
-
-// app.listen(port, () => {
-// 	console.log(`Example app listening at http://localhost:${port}`)
-// })
